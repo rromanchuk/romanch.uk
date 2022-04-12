d2b0119f3f048971732a4307e1825edc6ebf32c7
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-12 00:06:10 -0700

    cp

--- deploy_scripts/migrate.sh
+++ deploy_scripts/migrate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-export PATH="./bin:/home/ubuntu/.rbenv/shims:/home/ubuntu/.rbenv/bin:/bin:$PATH"
+#export PATH="./bin:/home/ubuntu/.rbenv/shims:/home/ubuntu/.rbenv/bin:/bin:$PATH"
 source /home/ubuntu/.bashrc
 printenv
 
