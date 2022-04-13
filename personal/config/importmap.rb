8a3b663168004b472575982559cd28c3890be01a
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-12 21:02:25 -0700

    cp

--- personal/config/importmap.rb
+++ personal/config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "firebase/app", to: "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js"
+pin "firebase/auth", to: "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
+pin "firebase/analytics", to: "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js"
