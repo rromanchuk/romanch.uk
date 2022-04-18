d42fb6ee22a2c3d547123b8fe16fd8b7385f3da9
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-18 01:38:02 -0700

    cp

--- personal/app/controllers/application_controller.rb
+++ personal/app/controllers/application_controller.rb
@@ -8,8 +8,39 @@ class ApplicationController < ActionController::Base
   def amzn_oidc_data() = request.headers['x-amzn-oidc-data']
   def client_ip() = request.headers['true-client-ip']
   def user_agent() = request.headers['User-Agent']
-
   def config() = Rails.configuration.general
+
+  def current_user
+    return @current_user if instance_variable_defined?(:@current_user)
+
+    find_by_session || find_by_alb
+  end
+
+  def find_by_session
+    User.find_by(id: session[:user_id])&.tap do |user|
+      set_current_user(user)
+    end
+  end
+
+  def find_by_alb
+    return nil unless amzn_oidc_identity
+    
+    User.create_with({}).find_or_create_by!(cognito_id: amzn_oidc_identity).tap do |user|
+      set_current_user(user)
+    end
+  end
+
+  def set_current_user(user)
+    @current_user = user
+    session[:user_id] = user.id
+  end
+
+  def logout!
+    cookies.delete '_alb_personal'
+    session[:user_id] = nil
+  end
+
+
 
   def append_info_to_payload(payload)
     super
--- personal/app/controllers/sessions_controller.rb
+++ personal/app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
   def logout
-    cookies.delete 'AWSELBAuthSessionCookie-0'
+    cookies.delete '_alb_personal'
     reset_session
     redirect_to "#{config.auth_url}?client_id=#{Credentials[:cognito_client]}", allow_other_host: true
+  end
+
+  def login
+    #"https://<your_domain>/login?response_type=code&client_id=<your_app_client_id>&redirect_uri=<your_callback_url>"
   end
 end
