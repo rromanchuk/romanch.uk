49c2a3e1745eabc96a9dcf55dfc64aca2b3755b5
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-17 21:35:23 -0700

    cp

--- personal/config/environments/production.rb
+++ personal/config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.lograge.enabled = true
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
 end
