08f533bdf0115e4bbdef4f8695174893884cbfcc
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-20 18:38:36 -0700

    cp

--- personal/app/javascript/application.js
+++ personal/app/javascript/application.js
@@ -9,6 +9,7 @@ import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faLinkedin, faStackOverflow, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
+
 config.mutateApproach = 'sync'
 library.add(faLinkedin, faStackOverflow, faGithub)
 dom.watch()
