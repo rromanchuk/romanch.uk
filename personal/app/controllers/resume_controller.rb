46499d156a889c3466d0ba4c49863c9a62af3543
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-26 01:42:37 -0700

    ci

--- personal/app/controllers/resume_controller.rb
+++ personal/app/controllers/resume_controller.rb
@@ -2,7 +2,7 @@
 class ResumeController < ApplicationController
   layout 'resume'
   let(:source) { File.open(File.join(Rails.root, 'app', 'views', 'resume', 'source.tex')).read }
-  let(:pdf) { File.open(File.join(Rails.root, 'app', 'assets', 'no_digesting', 'romanchuk.pdf')).read }
+  let(:pdf) { File.open(File.join(Rails.root, 'app', 'assets', 'documents', 'romanchuk.pdf')).read }
   
   def index; end
 
--- personal/app/javascript/controllers/search_controller.js
+++ personal/app/javascript/controllers/search_controller.js
@@ -1,9 +1,21 @@
 
 import { Controller } from "@hotwired/stimulus"
-import 'https://esm.run/video.js';
+
 
 export default class extends Controller {
+  static targets = ["form"];
   connect() {
     console.log("connect")
+  }
+
+  submit() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this._submit();
+    }, 300);
+  }
+
+  _submit() {
+    this.formTarget.requestSubmit();
   }
 }
