ec022a05d0332a1fc3dbb6f21d0b7d60c80901d6
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-28 02:11:48 -0700

    ci

--- personal/app/assets/config/manifest.js
+++ personal/app/assets/config/manifest.js
@@ -1,9 +1,10 @@
 //= link_tree ../images
 //= link_tree ../videos
-//= link_tree ../documents
+
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../../../vendor/images
 
 //= link_tree ../builds
 //= link favicon/browserconfig.xml
+//= link romanchuk.pdf
