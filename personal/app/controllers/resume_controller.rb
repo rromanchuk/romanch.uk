450285cf97334a38707f6e168019b9f97b158aa7
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-28 22:17:05 -0700

    ci

--- personal/app/controllers/resume_controller.rb
+++ personal/app/controllers/resume_controller.rb
@@ -3,12 +3,7 @@ class ResumeController < ApplicationController
   layout 'fluid'
 
   let(:source) { File.open(File.join(Rails.root, 'app', 'views', 'resume', 'source.tex')).read }
-
-
-
   def index; end
-
-  def video; end
 
   def turbo_frame_request_variant
     request.variant = :turbo_frame if turbo_frame_request?
