052dd8c619b79178eb0ba44c38c5cabf1f536064
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-28 23:01:50 -0700

    ci

--- deploy_scripts/compile_assets.sh
+++ deploy_scripts/compile_assets.sh
@@ -6,5 +6,5 @@ printenv
 cd /var/www/romanch.uk/codedeploy
 bin/rails assets:precompile
 aws s3 sync public/assets s3://personal-www/assets --storage-class INTELLIGENT_TIERING --cache-control max-age=31536000
-
+aws s3 sync public/videos s3://personal-www/videos --storage-class INTELLIGENT_TIERING --cache-control max-age=31536000
 exit 0
