3e093996eaf03ddd49d901a9762c1bfef83f55e1
Ryan Romanchuk <rromanchuk@gmail.com>
2022-04-29 01:04:07 -0700

    ci

--- deploy_scripts/compile_assets.sh
+++ deploy_scripts/compile_assets.sh
@@ -6,5 +6,21 @@ printenv
 cd /var/www/romanch.uk/codedeploy
 bin/rails assets:precompile
 aws s3 sync public/assets s3://personal-www/assets --storage-class INTELLIGENT_TIERING --cache-control max-age=31536000
-aws s3 sync public/videos s3://personal-www/videos --storage-class INTELLIGENT_TIERING 
+
+
+aws s3 sync public/videos s3://personal-www/videos \
+ --exclude '*' --include '*audio.mp4' \
+ --content-type 'audio/mp4'
+
+aws s3 sync public/videos s3://personal-www/videos \
+ --exclude '*' --include '*.m3u8' \
+ --content-type='application/vnd.apple.mpegurl' 
+
+aws s3 sync public/videos s3://personal-www/videos \
+ --exclude '*' --include '*vc.mp4' \
+ --content-type='video/mp4' 
+
+aws s3 sync public/videos s3://personal-www/videos \
+ --exclude '*' --include '*.jpg'
+
 exit 0
