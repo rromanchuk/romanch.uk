792bb7e095d9a0fbff4f4614401ba41f728cce17
Ryan Romanchuk <rromanchuk@gmail.com>
2022-05-17 19:19:14 -0700

    ci

---
+++ client/fiddle/fiddle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fiddle
+//
+//  Created by Ryan Romanchuk on 5/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ client/fiddle/fiddle/fiddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiddleApp.swift
+//  fiddle
+//
+//  Created by Ryan Romanchuk on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct fiddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
