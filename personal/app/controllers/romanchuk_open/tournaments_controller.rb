d2f2036a7edd047c4323cb16edcd1f300d5d2491
Ryan Romanchuk <rromanchuk@gmail.com>
2022-05-22 19:02:58 -0700

    ci

--- personal/app/controllers/romanchuk_open/tournaments_controller.rb
+++ personal/app/controllers/romanchuk_open/tournaments_controller.rb
@@ -1,12 +1,13 @@
 module RomanchukOpen
   class TournamentsController < RomanchukOpen::ApplicationController
     let(:tournament) { Tournament.friendly.find params[:id] }
-    let(:tournaments) { Tournament.all }
+    let(:tournaments) { Tournament.oldest(:ocurred_on) }
     def new
       @tournament = Tournament.new
     end
 
     def create
+      require_me!
       @tournament = Tournament.new(tournament_params)
       if tournament.save
         redirect_to romanchuk_open_tournaments_path
@@ -16,11 +17,13 @@ module RomanchukOpen
     end
 
     def destroy
+      require_me!
       tournament.destroy
       redirect_to romanchuk_open_tournaments_path, status: 303
     end
 
     def update
+      require_me!
       tournament.update(tournament_params)
       redirect_to romanchuk_open_tournament_path(tournament), status: 303
     end
