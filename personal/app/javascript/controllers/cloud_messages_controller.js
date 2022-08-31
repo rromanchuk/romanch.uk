e5ecf7b536f4df430261213fdbb34973fe148243
Ryan Romanchuk <rromanchuk@gmail.com>
2022-08-30 19:18:06 -0700

    cp

--- personal/app/javascript/controllers/cloud_messages_controller.js
+++ personal/app/javascript/controllers/cloud_messages_controller.js
@@ -1,21 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
-import { getMessaging, getToken } from "firebase/messaging";
+import { getMessaging, getToken, onMessage} from "firebase/messaging";
 import { firebase, VAPID_KEY } from "application";
 
 export default class extends Controller {
-  
+  static targets = [ "token" ]
   connect() {
     console.log('fcm controller')
+    this.tokenTarget.innerHTML = document.cookie;
+    this.messaging = getMessaging(firebase);
+    onMessage(this.messaging, (payload) => {
+      console.log('Message received. ', payload);
+    });
   }
 
   currentToken() {
-    const messaging = getMessaging(firebase);
-    getToken(messaging, { vapidKey: VAPID_KEY }).then((currentToken) => {
+    getToken(this.messaging, { vapidKey: VAPID_KEY }).then((currentToken) => {
       if (currentToken) {
         // Send the token to your server and update the UI if necessary
         // ...
         console.log('fcm token', currentToken);
         document.cookie = "fcm=" + currentToken + "; SameSite=Lax; Secure";
+        this.tokenTarget.innerHTML = document.cookie;
       } else {
         // Show permission request UI
         console.log('No registration token available. Request permission to generate one.');
--- personal/public/firebase-messaging-sw.js
+++ personal/public/firebase-messaging-sw.js
@@ -37,15 +37,15 @@ firebase.initializeApp(firebaseConfig);
 // and you should use data messages for custom notifications.
 // For more info see: 
 // https://firebase.google.com/docs/cloud-messaging/concept-options
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
+// messaging.onBackgroundMessage(function(payload) {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     body: 'Background Message body.',
+//     icon: '/firebase-logo.png'
+//   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
