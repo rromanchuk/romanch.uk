10e5393cf3d99987f90140d57f0e71206acc8dfe
Ryan Romanchuk <rromanchuk@gmail.com>
2022-08-30 17:59:49 -0700

    - Experiment with direct to provider oauth2 - Skip cognito idp interstitial by passing `identity_provider` - make sure ALB terminated and local still work

--- personal/app/components/modal_component.rb
+++ personal/app/components/modal_component.rb
@@ -3,9 +3,10 @@
 class ModalComponent < ViewComponent::Base
   renders_one :body
 
-  attr_reader :target, :title, :size
-  def initialize(title: '', size: 'modal-fullscreen')
+  attr_reader :target, :title, :size, :close_footer
+  def initialize(title: '', size: 'modal-fullscreen', close_footer: true)
     @size = size
     @title = title
+    @close_footer = close_footer
   end
 end
