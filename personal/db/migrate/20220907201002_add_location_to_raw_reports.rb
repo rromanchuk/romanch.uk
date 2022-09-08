5516a9235a1e2efdc91d9de4daeb57b4aa7c5fda
Ryan Romanchuk <rromanchuk@gmail.com>
2022-09-07 18:23:53 -0700

    fix

--- personal/db/migrate/20220907201002_add_location_to_raw_reports.rb
+++ personal/db/migrate/20220907201002_add_location_to_raw_reports.rb
@@ -1,5 +1,6 @@
 class AddLocationToRawReports < ActiveRecord::Migration[7.0]
   def change
+    enable_extension :postgis
     add_column :pireps_raw_reports, :location, :st_point, geographic: true, has_z: true
   end
 end
