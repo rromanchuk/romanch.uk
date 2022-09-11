819b7b6673326178f3e63a1ad8f6fb7a4109abc5
Ryan Romanchuk <rromanchuk@gmail.com>
2022-09-10 20:01:11 -0700

    scheudle

--- personal/config/schedule.rb
+++ personal/config/schedule.rb
@@ -7,6 +7,11 @@ every 1.day do
   runner 'PgHero.capture_space_stats'
 end
 
+every 5.minutes do
+  runner 'Wx::Pireps::Ingest.call'
+  # runner 'Wx::Metars::Ingest.call'
+end
+
 # every 30.minutes do
 #   runner 'Pireps::Ingest.call'
 # end
