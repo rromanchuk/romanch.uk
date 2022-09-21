478e5451ade677f16db53149c6a45cd6218873ab
Ryan Romanchuk <rromanchuk@gmail.com>
2022-09-20 22:57:47 -0700

    ci

--- personal/config/schedule.rb
+++ personal/config/schedule.rb
@@ -1,17 +1,19 @@
+job_type :crunner, "cd /var/www/romanch.uk/current && bin/rails runner -e :environment ':task' :output"
+
 every 5.minutes do # 1.minute 1.day 1.week 1.month 1.year is also supported
   # the following tasks are run in parallel (not in sequence)
-  runner 'PgHero.capture_query_stats'
+  crunner 'PgHero.capture_query_stats'
 end
 
 every 1.day do
-  runner 'PgHero.capture_space_stats'
+  crunner 'PgHero.capture_space_stats'
 end
 
 every 15.minutes do
-  runner 'Wx::Pireps::Ingest.call'
+  crunner 'Wx::Pireps::Ingest.call'
 end
 
 every 30.minutes do
-  runner 'Wx::Metars::Ingest.call'
-  runner 'Wx::Tafs::Ingest.call'
+  crunner 'Wx::Metars::Ingest.call'
+  crunner 'Wx::Tafs::Ingest.call'
 end
