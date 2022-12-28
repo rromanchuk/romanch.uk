3bd117cc16e0e935de77eb6a792fa4422fe89a90
Ryan Romanchuk <ryan@romanch.uk>
2022-12-28 14:40:51 -0700

    ci

--- personal/db/migrate/20221222081620_pireps_gin4.rb
+++ personal/db/migrate/20221222081620_pireps_gin4.rb
@@ -2,7 +2,7 @@ class PirepsGin4 < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
   def change
 
-    add_index :wx_pireps, :searchable_tsearch, using: :gin, algorithm: :concurrently
-    remove_index :wx_pireps, :tsvector_remarks_tsearch
+    # add_index :wx_pireps, :searchable_tsearch, using: :gin, algorithm: :concurrently
+    # remove_index :wx_pireps, :tsvector_remarks_tsearch
   end
 end
