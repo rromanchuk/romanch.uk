2d3fa6b41f0fbd97cb53a406d851e909125c86c7
Ryan Romanchuk <ryan@romanch.uk>
2023-03-02 19:33:21 -0600

    ci

--- personal/app/controllers/wx_station_observations_controller.rb
+++ personal/app/controllers/wx_station_observations_controller.rb
@@ -1,5 +1,5 @@
 class WxStationObservationsController < ApplicationController
-  let(:weather_station_observations) do
+  let(:wxr_station_observations) do
     relation = WxStationObservation.recent
     @dr_pagy, records = pagy_countless(relation, items: 50)
     records
