7d42135a5e8cda218b6d9a1f6834176ea9323ff7
Ryan Romanchuk <ryan@romanch.uk>
2023-05-29 13:37:34 -0500

    ci

--- personal/app/controllers/ro/images_controller.rb
+++ personal/app/controllers/ro/images_controller.rb
@@ -8,18 +8,31 @@ module Ro
       records
     end
     
+    def index
+      require_me!
+    end
+
     def new
+      require_me!
       @image = Image.new
     end
 
     def create
+      require_me!
       @image = Image.create!(image_params)
       redirect_to request.referer, status: :see_other
     end
 
     def update
+      require_me!
       image.update!(image_params)
       redirect_to request.referer, status: :see_other
+    end
+
+    def destroy
+      require_me!
+      image.destroy
+      redirect_to  ro_images_path, status: :see_other
     end
     
     private
