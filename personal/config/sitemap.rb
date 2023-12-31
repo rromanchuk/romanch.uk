9d66063907a49b7e2332c55f47bbdd27e210f7ea
Ryan Romanchuk <ryan@romanch.uk>
2023-12-31 01:38:29 -0600

    ci

--- personal/config/sitemap.rb
+++ personal/config/sitemap.rb
@@ -4,27 +4,27 @@ SitemapGenerator::Sitemap.verbose = true
 SitemapGenerator::Sitemap.compress = false
 # SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.create(default_host: 'https://romanch.uk', sitemaps_path: 'sitemaps/romanchuk') do
-  Post.published.find_each do |post|
-    add(post_path(post), lastmod: post.updated_at, changefreq: :monthly, priority: 0.7)
-  end
+  # Post.published.find_each do |post|
+  #   add(post_path(post), lastmod: post.updated_at, changefreq: :monthly, priority: 0.7)
+  # end
 
-  Blob.images.find_each do |blob|
-    add(serve_image_path(blob.slug), images: [{
-          loc: blob.key
-        }], lastmod: blob.updated_at)
-  end
+  # Blob.images.find_each do |blob|
+  #   add(serve_image_path(blob.slug), images: [{
+  #         loc: blob.key
+  #       }], lastmod: blob.updated_at)
+  # end
 
-  Blob.videos.find_each do |blob|
-    add(serve_video_path(blob.slug), video: {
-          title: blob.title,
-          description: blob.description,
-          thumbnail_loc: blob.thumbnail_key,
-          content_loc: blob.key,
-          publication_date: blob.created_at.to_s,
-          live: false,
-          tags: blob.tag_names
-        }, lastmod: blob.updated_at)
-  end
+  # Blob.videos.find_each do |blob|
+  #   add(serve_video_path(blob.slug), video: {
+  #         title: blob.title,
+  #         description: blob.description,
+  #         thumbnail_loc: blob.thumbnail_key,
+  #         content_loc: blob.key,
+  #         publication_date: blob.created_at.to_s,
+  #         live: false,
+  #         tags: blob.tag_names
+  #       }, lastmod: blob.updated_at)
+  # end
 end
 
 # SitemapGenerator::Sitemap.create(default_host: 'https://romanchukopen.com', sitemaps_path: 'sitemaps/romanchukopen') do
