c35dea79637d7edbe2de09b8aca8e85a30a68fd6
Ryan Romanchuk <ryan@romanch.uk>
2025-01-10 01:36:52 -0600

    ci

--- personal/config/environments/production.rb
+++ personal/config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/healthcheck" } }
-  config.action_cable.allowed_request_origins = ["https://romanch.uk"]
+  config.action_cable.allowed_request_origins = ["https://romanch.uk", "http://romanch.uk"]
   
   logger           = ActiveSupport::Logger.new(STDOUT)
   logger.formatter = config.log_formatter
