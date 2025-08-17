323a4cbb1619c1050bfaefe1abcc545ae99328f0
Ryan Romanchuk <ryan@romanch.uk>
2025-08-17 14:57:25 -0500

    cleanup

--- app/models/tds/aircraft_type_designator.rb
+++ app/models/tds/aircraft_type_designator.rb
@@ -1,10 +1,11 @@
 module Tds
   class AircraftTypeDesignator < ApplicationRecord
+    include PgSearch::Model
     include Sluggable
-    #include Searchable
+    include Searchable
     SEARCH_AGAINST = %i[icao_code manufacturer model]
 
-    pg_search_scope :search, against: %i[icao_code manufacturer model]
+    pg_search_scope :search, against: SEARCH_AGAINST
 
     store_accessor :data, :other_models
     def slug_candidates = %i[icao_code manufacturer model]
