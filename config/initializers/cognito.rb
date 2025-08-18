a4119f1a46c9351f011b97b11b759ce6130630bd
Ryan Romanchuk <ryan@romanch.uk>
2025-08-17 19:34:00 -0500

    remove dead

--- config/initializers/cognito.rb
+++
@@ -1,5 +0,0 @@
-# CognitoTokenVerifier.configure do |config|
-#   config.aws_region = 'us-east-1'
-#   config.user_pool_id = Credentials[:cognito_user_pool]
-#   config.token_use = 'id' # acceptable options are 'id' and 'access'; can be an array for both options - defaults to allowing either cognito token type
-# end
